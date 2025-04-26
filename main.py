9fed065dab7bbe111e15a3e4a985c44259380372
M <73540043+miha128@users.noreply.github.com>
2025-04-26 14:19:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import time
 
 start_time = time.time()
 message_count = 0
-
-BOT_TOKEN = "MTM.."
+# bot token goes here
+BOT_TOKEN = "MTM..."
 
 command_name = "ask"
 command_desc = "Ask the AI a question"
@@ -17,13 +17,12 @@ command_desc = "Ask the AI a question"
 # change with your preferred openai compatible API
 base_url = "http://localhost:1234/v1" # for example, here i used LM Studio's server
 model = "motoreta"
-api_key = "API key here"
+api_key = "API key here" # not needed for local LLM, just make sure it's not empty if you're using a local one or leave as default
 
 beforeprompt = f"Today's date: {dt.now().strftime('%B %d %Y')}. You are currently roleplaying as a helpful robot which will respond to a user's query. Keep your answers short. Be clear and concise. The user's query is: "
 
 my_client = discord.Client(intents=discord.Intents.default())
 tree = app_commands.CommandTree(my_client)
-# For LMS / literally any self hosted AI client the api key isn't really needed and can be left as default
 openai_client = AsyncOpenAI(base_url=base_url, api_key=api_key)
 
 @tree.command(
@@ -38,10 +37,13 @@ async def info(interaction: discord.Interaction):
     uptime_str = str(timedelta(seconds=uptime_seconds)).split(".")[0]
     #embed
     embed = discord.Embed(
-        title="Bot info:",
+        title= f"Statistics as of {dt.now().strftime('%B %d %Y')}:",
         color=discord.Color.blue()
     )
-    
+    embed.set_author(name="Powered by slashcord",
+                 url="https://github.com/miha128/slashcord",
+                 icon_url="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png")
+                 
     embed.add_field(
         name="Server",
         value=f"**Endpoint:** <{base_url}> \n"
@@ -62,8 +64,6 @@ async def info(interaction: discord.Interaction):
               f"**Messages Processed:** {message_count}",
         inline=False
     )
-    
-    embed.set_footer(text=f"https://github.com/miha128/slashcord \nSystem time: {dt.now().strftime('%B %d %Y')}")
     
     await interaction.response.send_message(embed=embed)
     
